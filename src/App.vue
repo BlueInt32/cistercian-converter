5932de44f6c9a7f3af8554c1e3d89d7a32964269
BlueInt32 <simon.budin@gmail.com>
2021-02-06 07:18:52 +0100

    Make svg clever use (scale, transform, viewbox)

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <template>
-  <a class="github-link" target="_blank" href="https://github.com/BlueInt32/cistercian-converter">
+  <a id="github-link" target="_blank" href="https://github.com/BlueInt32/cistercian-converter">
     <img src="@/assets/github.png" alt="github" />
   </a>
-  <div class="input-zone">
-    <p>Pick a number :</p>
-    <input type="number" v-model="number" id="input-number" min="0" max="9999" @input="compute" />
-  </div>
-  <div class="svg-zone">
-    <svg width="600" height="600">
-      <g>
-        <polyline
-          :points="`300,${stroke / 2} 300,${600 - stroke}`"
-          stroke="black"
-          :stroke-width="stroke"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <polyline
-          v-for="(line, index) in lines"
-          :key="index"
-          :points="`${line[0][0]},${line[0][1]} ${line[1][0]},${line[1][1]}`"
-          fill="none"
-          stroke="black"
-          :stroke-width="stroke"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </g>
-    </svg>
-  </div>
+  <p>Pick a number</p>
+  <input type="number" v-model="number" id="input-number" min="0" max="9999" @input="compute" />
+  <!-- viewbox attribute maps the available svg element space to local coordinates. Here the center is at top center-->
+  <svg viewBox="-6 -1 12 14">
+    <g>
+      <polyline points="0,0 0,12" />
+      <polyline v-for="(line, index) in lines" :transform="line.transform" :key="index" :points="line.points" />
+    </g>
+  </svg>
 </template>
 
 <script lang="ts">
@@ -38,78 +20,37 @@ import { Options, Vue } from 'vue-class-component';
   components: {}
 })
 export default class App extends Vue {
-  stroke = 15;
-
-  zero = this.stroke / 2;
+  lines: { points: string; transform: string }[] = [];
 
   number = 1983;
 
   digits = [0, 0, 0, 0];
 
-  xMiddle = 300;
-
-  xOffset = 200;
-
-  yOffset = 200;
-
-  a = [this.xMiddle, this.zero];
-
-  b = [this.xMiddle + this.xOffset, this.zero];
-
-  c = [this.xMiddle, this.yOffset];
-
-  d = [this.xMiddle + this.xOffset, this.yOffset];
-
-  map2: { [index: number]: number[][][] } = {
-    0: [],
-    1: [[this.a, this.b]],
-    2: [[this.c, this.d]],
-    3: [[this.a, this.d]],
-    4: [[this.c, this.b]],
-    5: [
-      [this.a, this.b],
-      [this.c, this.b]
-    ],
-    6: [[this.b, this.d]],
-    7: [
-      [this.a, this.b],
-      [this.b, this.d]
-    ],
-    8: [
-      [this.b, this.d],
-      [this.c, this.d]
-    ],
-    9: [
-      [this.a, this.b],
-      [this.b, this.d],
-      [this.c, this.d]
-    ]
+  transformMap: { [index: number]: string } = {
+    0: 'translate(0, 12) scale(-1,-1) ',
+    1: 'translate(0, 12) scale(1,-1) ',
+    2: 'scale(-1,1)',
+    3: ''
   };
 
-  // https://developer.mozilla.org/en-US/docs/Web/SVG/Element
-  lines: number[][][] = [];
+  map: { [index: number]: string } = {
+    0: '',
+    1: '0,0 4,0',
+    2: '0,4 4,4',
+    3: '0,0 4,4',
+    4: '0,4 4,0',
+    5: '0,0 4,0 0,4',
+    6: '4,0 4,4',
+    7: '0,0 4,0 4,4',
+    8: '4,0 4,4 0,4',
+    9: '0,0 4,0 4,4 0,4'
+  };
 
   compute() {
     this.getDigits();
-    this.xOffset = 200;
-    const unitLines = JSON.parse(JSON.stringify(this.map2[this.digits[3]]));
-    const tensLines = JSON.parse(JSON.stringify(this.map2[this.digits[2]]));
-    tensLines.forEach((line: number[][]) => {
-      line[0] = this.mirrorXPoint(line[0]);
-      line[1] = this.mirrorXPoint(line[1]);
-    });
-    const hundredsLines = JSON.parse(JSON.stringify(this.map2[this.digits[1]]));
-    hundredsLines.forEach((line: number[][]) => {
-      line[0] = this.offsetYPoint(this.mirrorYPoint(line[0]));
-      line[1] = this.offsetYPoint(this.mirrorYPoint(line[1]));
-    });
-
-    const thousandsLines = JSON.parse(JSON.stringify(this.map2[this.digits[0]]));
-    thousandsLines.forEach((line: number[][]) => {
-      line[0] = this.mirrorXPoint(this.offsetYPoint(this.mirrorYPoint(line[0])));
-      line[1] = this.mirrorXPoint(this.offsetYPoint(this.mirrorYPoint(line[1])));
-    });
-    this.lines = unitLines.concat(tensLines).concat(hundredsLines).concat(thousandsLines);
+    this.lines = this.digits
+      .map((d, i) => ({ points: this.map[d], transform: this.transformMap[i] }))
+      .filter((p) => p.points);
   }
 
   getDigits() {
@@ -118,18 +59,6 @@ export default class App extends Vue {
       .padStart(4, '0')
       .split('')
       .map((d) => parseInt(d, 10));
-  }
-
-  mirrorXPoint(point: number[]) {
-    return [-point[0] + 2 * this.xMiddle, point[1]];
-  }
-
-  mirrorYPoint(point: number[]) {
-    return [point[0], -point[1]];
-  }
-
-  offsetYPoint(point: number[]) {
-    return [point[0], point[1] + 600 - this.stroke / 2];
   }
 
   mounted() {
@@ -145,49 +74,62 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
-h1 {
-  font-size: 5em;
-}
 body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+}
+h1 {
+  font-size: 1.8em;
+  font-variant: small-caps;
+  margin: 0.8em;
+}
+
+p {
+  font-variant: small-caps;
+  font-weight: 600;
 }
 
 #app {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 
 #input-number {
-  font-size: 6em;
-  width: 270px;
-  border: 0;
-  // &:focus {
-  //   border: 1px solid red;
-  // }
-}
-.input-zone {
-  display: flex;
-  align-items: center;
+  font-size: 4em;
+  margin: 0 auto;
+  text-align: center;
 
-  p {
-    margin-right: 2em;
+  border: 8px solid black;
+  border-radius: 20px;
+  &:focus {
+    outline: none;
   }
 }
 
-.svg-zone {
-  margin: 2em;
-  // border: 4px solid black;
+svg {
+  margin: 3em auto;
+  height: 50vh;
+  width: 50vw;
 }
-.github-link {
+
+#github-link {
   display: inline-block;
   position: absolute;
   top: 0.5em;
   right: 0.5em;
+}
+polyline {
+  stroke-width: 0.7;
+  fill: none;
+  stroke: black;
+  stroke-linecap: round;
+  stroke-linejoin: round;
 }
 </style>
